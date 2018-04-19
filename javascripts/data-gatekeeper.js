0abaa55d177d614282568438d4525b2fe6071800
Alicia Zaludova <aliciazaludova@gmail.com>
2018-04-18 20:13:37 -0500

    printing categories to dom

--- javascripts/data-gatekeeper.js
+++ javascripts/data-gatekeeper.js
@@ -1,5 +1,6 @@
 const loadCategories = require('./xhr-categories');
 const loadElements = require('./xhr-elements');
+const printCategoriesToDom = require('./dom-main');
 
 const whenElementsLoad = function () {
   const elementsData = JSON.parse(this.responseText).elements;
@@ -9,6 +10,7 @@ const whenElementsLoad = function () {
 const whenCategoriesLoad = function () {
   const categoriesData = JSON.parse(this.responseText).categories;
   console.log(categoriesData);
+  printCategoriesToDom(categoriesData);
 };
 
 const errorFunction = function () {
--- javascripts/dom-main.js
+++ javascripts/dom-main.js
@@ -1,3 +1,19 @@
 const categoriesOutputDiv = document.getElementById('categories');
 
 console.log(categoriesOutputDiv);
+
+const domStrang = (categories) => {
+  let domString = '';
+  categories.forEach((category) => {
+    domString += `<div class="col-sm-12">`;
+    domString +=   `<h3>${category.name}</h3>`;
+    domString += `</div>`;
+  });
+  return domString;
+};
+
+const printCategoriesToDom = (categories) => {
+  categoriesOutputDiv.innerHTML = domStrang(categories);
+};
+
+module.exports = printCategoriesToDom;
